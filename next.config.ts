a883377ecd11d97599841ac29df7f952fd7d2916
Sergei Rahouski <sergei.rahouski@oracle.com>
2025-02-28 18:48:16 -0600

    Adding newrelic

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const nrExternals = require('newrelic/load-externals')
+
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['newrelic'],
   /* config options here */
+  webpack: (config) => {
+    nrExternals(config)
+    return config
+  }
 };
 
 export default nextConfig;
